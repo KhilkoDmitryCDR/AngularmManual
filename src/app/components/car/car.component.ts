b462c88f57d58dcae73f2b192f59ed6501992e38
Dmitry Khilko <dmitry.jilko@gmail.com>
2021-09-21 12:11:14 +0200

    Eventually my first component

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -13,6 +13,7 @@ export class CarComponent implements OnInit {
   colors!:Colors;
   options!:string[];
   test:any;
+  
 
 
   constructor() { }
@@ -28,8 +29,62 @@ export class CarComponent implements OnInit {
 
     };
     this.options= ["ABS","Auto-parcking"];
-    this.test=1;
+    
   }
+
+  addOpt(option:string){
+    this.options.unshift(option);
+    return false;
+
+  }
+  deleteOpt(option:string){
+    for(let i=0;i<this.options.length; i++){
+      if(this.options[i]==option)
+        this.options.splice(i,1);
+        break;
+      
+    }
+  }
+
+  carSelect(carName: string){
+    if(carName== 'bmw'){
+    this.name="BMW";
+    this.speed=280;
+    this.model="M5";
+    this.colors={
+      car: 'BLACK',
+      salon: 'Black',
+      wheels:'Silver'
+
+    };
+    this.options= ["ABS","Auto-parcking"];
+   
+  }else if(carName=='audi'){
+    this.name="Audi";
+    this.speed=235;
+    this.model="RS8";
+    this.colors={
+      car: 'White',
+      salon: 'Black',
+      wheels:'Silver'
+
+    };
+    this.options= ["ABS","Auto-parcking"];
+   
+  }else{
+    this.name="Mercedes";
+    this.speed=180;
+    this.model="C180";
+    this.colors={
+      car: 'White',
+      salon: 'Black',
+      wheels:'Silver'
+
+    };
+    this.options= ["ABS","Auto-parcking"];
+    
+  }
+}
 
 }
 interface Colors{
